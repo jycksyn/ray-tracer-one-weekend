448e2f09a45f41962c12b9e7c880a0c489a8f3be
jackson <jackson.kubin@gmail.com>
2023-12-24 11:49:57 -0500

    create vec3

--- src/vec3.h
+++ src/vec3.h
@@ -65,3 +65,79 @@ inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
   return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline vec3 operator*(float t, const vec3 &v1) {
+    return vec3(t * v1.e[0], t * v1.e[1], t * v1.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, float t) {
+    return vec3(t * v1.e[0], t * v1.e[1], t * v1.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, float t) {
+    return vec3(v1.e[0] / t, v1.e[1] / t, v1.e[2] / t);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[3] * v2.e[3]
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec3(
+            v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
+            -v1.e[0] * v2.e[2] + v1.e[2] * v2.e[0],
+            v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
+            );
+}
+
+inline vec3& vec3::operator+=(const vec3 &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v) {
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3 &v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const float t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const float k) {
+    float t = 1.0/k;
+
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
