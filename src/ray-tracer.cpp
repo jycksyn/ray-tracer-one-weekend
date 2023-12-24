f032c1aa406a18147d8b07962cd9c292af70228e
Jackson Kubin <jpkubin@wm.edu>
2023-12-23 19:57:15 -0500

    use stb_image

--- src/ray-tracer.cpp
+++ src/ray-tracer.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
+#include<vector>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 using namespace std;
 
 int main() {
     int nx = 200;
     int ny = 100;
-    cout << "P3\n" << nx << " " << ny << "\n255\n";
+    char * pixels = new char[nx * ny * 3];
+    int i = 0;
     for (int y = ny - 1; y >= 0; y--) {
         for (int x = 0; x < nx; x++) {
             float r = float(x) / float(nx);
             float g = float(y) / float(ny);
             float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-            cout << ir << " " << ig << " " << ib << " "; 
+            pixels[i++] = char(255.99 * r);
+            pixels[i++] = char(255.99 * g);
+            pixels[i++] = char(255.99 * b);
         }
     }
+    stbi_write_png("foo.png", nx, ny, 3, pixels, 3 * nx);
 }
